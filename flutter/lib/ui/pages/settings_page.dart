89f882e00fa0292336e8c88eb9c728a95350406a
Reid Lai <reidlai@gmail.com>
2026-09-07 22:00:48 +0800

    fix(settings): restrict tap interaction strictly to Switch widget on Simulator row

--- flutter/lib/ui/pages/settings_page.dart
+++ flutter/lib/ui/pages/settings_page.dart
@@ -123,9 +123,6 @@ class SettingsPage extends StatelessWidget {
                           leadingIcon: Icons.developer_board,
                           label: "Simulator",
                           showChevron: false,
-                          onTap: () {
-                            BleSimulatorDriver().setSimulatorEnabled(!isSimActive);
-                          },
                           trailingWidget: Switch(
                             value: isSimActive,
                             activeThumbColor: AppColors.accentGreen,
